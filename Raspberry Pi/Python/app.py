301938c8f9c08676f007574829e8105b35a2f6be
Girish Krishnan <gikrishnan@ucsd.edu>
2024-02-21 10:24:01 -0800

    Update localhost to 0.0.0.0 to make it accessible on the Durip computer

--- Raspberry Pi/Python/app.py
+++ Raspberry Pi/Python/app.py
@@ -41,4 +41,4 @@ async def view_schedules():
         raise HTTPException(status_code=400, detail=str(e))
     
 if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=8000, log_level="info")
+    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="info")
